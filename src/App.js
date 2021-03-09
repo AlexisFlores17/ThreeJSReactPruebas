3ba01d0f07ff61526639a627f0b7237e7efcba58
Alexis <alex.p.flores17@gmail.com>
2021-03-08 21:55:34 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@react-three/drei';
+import React from 'react';
+
+//first
+import {Canvas} from "react-three-fiber";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* //only threeJS elements */}
+      <Canvas>
+        {/* <mesh>
+          <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
+          <meshStandardMaterial attach='material' />
+        </mesh> */}
+        <Box />
+      </Canvas>
+      
+    </>
   );
 }
 
